2d333b415b14579adf19d339cdece91b236213f2
hamzamoha-a <hamzashukri29@gmail.com>
2024-10-09 18:00:09 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+function submitData(name, email) {
+    const configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email,
+        }),
+    };
+
+    return fetch("http://localhost:3000/users", configurationObject)
+        .then((response) => response.json())
+        .then((data) => {
+            const idElement = document.createElement("p");
+            idElement.innerText = `New user ID: ${data.id}`;
+            document.body.appendChild(idElement);
+        });
+}
+
